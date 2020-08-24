e5cca305902e0f4ab1487c2908dfc64ba6c4d298
samga <samgalaxy111111@gmail.com>
2020-08-24 09:47:45 +0700

    (Error) WorkManager request data

--- app/src/main/java/com/sam/myworkmanager/MainActivity.java
+++ app/src/main/java/com/sam/myworkmanager/MainActivity.java
@@ -1,14 +1,64 @@
 package com.sam.myworkmanager;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.work.Data;
+import androidx.work.OneTimeWorkRequest;
+import androidx.work.WorkInfo;
+import androidx.work.WorkManager;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button btnOneTimeTask;
+    EditText editCity;
+    TextView textStatus;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnOneTimeTask = findViewById(R.id.btnOneTimeTask);
+        editCity = findViewById(R.id.editCity);
+        textStatus = findViewById(R.id.textStatus);
+
+        btnOneTimeTask.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.btnOneTimeTask:
+                startOneTimeTask();
+                break;
+        }
+    }
+
+    private void startOneTimeTask(){
+        textStatus.setText(getString(R.string.status));
+
+        Data data = new Data.Builder()
+                .putString(MyWorker.EXTRA_CITY, editCity.getText().toString())
+                .build();
+
+        OneTimeWorkRequest oneTimeWorkRequest = new OneTimeWorkRequest.Builder(MyWorker.class)
+                .setInputData(data)
+                .build();
+
+        WorkManager.getInstance().enqueue(oneTimeWorkRequest);
+
+        WorkManager.getInstance().getWorkInfoByIdLiveData(oneTimeWorkRequest.getId()).observe(MainActivity.this, new Observer<WorkInfo>() {
+            @Override
+            public void onChanged(WorkInfo workInfo) {
+                String status = workInfo.getState().name();
+                textStatus.append("\n"+status);
+            }
+        });
     }
 }
